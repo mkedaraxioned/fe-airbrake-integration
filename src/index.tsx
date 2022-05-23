483a01c2fbd09bed81a8c9289011e78bd2fd9f68
idnyaneshwaraxioned <dnyaneshwari@axioned.com>
2022-05-23 11:15:26 +0530

    feat: redux persist setup done

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,20 @@ import { ChakraProvider, ColorModeScript, theme } from '@chakra-ui/react';
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
-import { store } from './store';
+import { persistor, store } from './store';
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript />
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </ChakraProvider>
   </React.StrictMode>,
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,37 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../feature/counterSlice'
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import counterReducer from '../feature/counterSlice';
+import persistReducer from 'redux-persist/es/persistReducer';
+import {
+  persistStore,
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
+
+const reducers = combineReducers({
+  counter: counterReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+const persistedReducer: any = persistReducer(persistConfig, reducers);
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer
-  },
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
