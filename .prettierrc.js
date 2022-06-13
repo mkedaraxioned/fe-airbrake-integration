0a703ff3f437ba635d0e0f23417a6d36fa5e3b3e
idnyaneshwaraxioned <dnyaneshwari@axioned.com>
2022-06-13 12:20:28 +0530

    feat: update husky files

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  jsxSingleQuote: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  // Override any other rules you want
+};
