4c1635e3daa99e121d16204c9d5dbaaa5696fdd7
Shubham D <shubhamd@axioned.com>
2022-08-23 13:17:50 +0530

    fix: differentiate dev and prod using env

--- src/constants/backend.ts
+++ src/constants/backend.ts
@@ -1,3 +1,7 @@
 export const variables = {
-  BACKEND_URL: 'https://loggr-api.axioned.com/',
+  BACKEND_URL: `${
+    process.env.NODE_ENV == 'development'
+      ? 'https://dev-loggr-api.axioned.com/'
+      : 'https://loggr-api.axioned.com/'
+  }`,
 };
